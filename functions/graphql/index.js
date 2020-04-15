2ccd96bbfb751b9ec617016d025871c7a858eb53
robert <dungeonry@hotmail.com>
2020-04-15 15:52:10 -0700

    first commit

---
+++ functions/graphql/index.js
@@ -0,0 +1,51 @@
+const { ApolloServer, gql } = require("apollo-server-lambda");
+
+const typeDefs = gql`
+    type Query{
+        todos: [Todo]!
+    }
+
+    type Todo{
+        id: ID!
+        text: String!
+        done: Boolean!
+    }
+
+    type Mutation{
+        addTodo(text: String!): Todo
+        updateTodoDone(id: ID!): Todo
+    }
+`;
+
+const todos = {
+
+}
+
+const resolvers = {
+    Query: {
+        hello: () => "Hello World",
+        todos: () => {
+            return Object.values(todos);
+        }
+    },
+    Mutation: {
+        addTodo: (_, { text }) => {
+            todoIndex++;
+            const id = `key-${todoIndex}`;
+            todos[id] = { id, text, done: false };
+        },
+        updateTodoDone: (_, { id }) => {
+            todos[id].done = true;
+            return todo[id];
+        }
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground: true,
+    introspection: true
+});
+
+module.exports = server.createHandler();
